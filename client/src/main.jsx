0fe4ff7276ff80a1d470904790d156c7066f9ed7
VietCT <transonviet2004@gmail.com>
2024-09-11 22:05:46 +0800

    Added Register page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Register from "./components/Register.jsx"
 import {
   createBrowserRouter,
   RouterProvider,
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   }
 ]);
 
