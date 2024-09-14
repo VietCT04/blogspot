106551c44a790e1a64186f25f030a546bf834cc5
VietCT <transonviet2004@gmail.com>
2024-09-15 00:03:15 +0800

    Fully added register page css

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -18,6 +18,9 @@ const Login = () => {
       });
 
       setMessage(response.data);
+      if (response.status == 200){
+        navigate('/home');
+      }
     } catch (error) {
       if (error.response) {
         setMessage(error.response.data);
@@ -28,31 +31,35 @@ const Login = () => {
   };
 
   return (
-    <div className='LoginForm'>
-      <div>Login</div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input 
-            type="text" 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input 
-            type="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
-          />
-        </div>
-        <button type="submit">Login</button>
-        <button onClick={() => navigate('/register')}>Register</button>
-      </form>
-      <div>{message}</div>
+    <div className='login-container'>
+      <div className='login-form'>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className='input-group'>
+            <label>Username:</label>
+            <input 
+              type="text"
+              placeholder='Username'
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required 
+            />
+          </div>
+          <div className='input-group'>
+            <label>Password:</label>
+            <input 
+              type="password" 
+              value={password} 
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+          </div>
+          <button type="submit">Login</button>
+          <button onClick={() => navigate('/register')}>Register</button>
+        </form>
+        <div className='login-message'>{message}</div>
+      </div>
     </div>
   );
 };
